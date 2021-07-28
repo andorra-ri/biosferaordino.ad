3101d4694d16939fe81cf871b4f499b9411a3ddc
mvilella <mvilella@obsa.ad>
2021-07-28 19:34:51 +0200

    Breakpoint for mobile navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,11 @@
 <template>
   <header v-scrolled:80="'scrolled'" class="navbar">
-    <div class="container">
+    <div class="container" :class="{ open }">
       <img src="/images/logo.svg" class="logo">
       <nav class="main-nav">
+        <label class="burger">
+          <input v-model="open" type="checkbox">
+        </label>
         <ul>
           <li><router-link to="/">Index</router-link></li>
           <li><router-link to="/descobreix">Descobreix</router-link></li>
@@ -16,13 +19,15 @@
 </template>
 
 <script>
+import { ref } from 'vue';
 import Scrolled from '/@/utils/directive.scrolled';
 
 export default {
   name: 'TopBar',
   directives: { Scrolled },
   setup() {
-    return {};
+    const open = ref(false);
+    return { open };
   },
 };
 </script>
@@ -33,13 +38,14 @@ export default {
   left: 0;
   top: 0;
   width: 100%;
-  padding: 1rem 0;
   color: #fff;
   transition: all 0.75s ease;
+  z-index: 2;
 
   .container {
     display: flex;
     align-items: center;
+    padding: 1rem;
   }
 
   .logo {
@@ -62,6 +68,7 @@ export default {
       padding: 0.75rem 1rem;
       text-decoration: none;
       border-bottom: 2px solid transparent;
+      position: relative;
 
       &.router-link-active { border-color: currentColor; }
     }
@@ -74,6 +81,54 @@ export default {
     .logo {
       height: 3rem;
       filter: brightness(0);
+    }
+  }
+
+  .burger { display: none; }
+}
+
+@media only screen and (min-width: 320px) and (max-width: 768px) {
+  .navbar {
+    .burger { display: block; }
+
+    .main-nav ul {
+      flex-direction: column;
+      justify-content: space-evenly;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      margin: 0;
+      padding: 8rem 2rem 4rem; // Must leave space for logo
+      background: #fff;
+      z-index: -1;
+      box-sizing: border-box;
+      font-size: 1.5rem;
+      display: none;
+
+      a.router-link-active {
+        border: 0;
+
+        &::before {
+          position: absolute;
+          top: 0;
+          left: 0;
+          content: '';
+          display: block;
+          height: 100%;
+          width: 4px;
+          background: #ff9f1b;
+          border-radius: 4px;
+        }
+      }
+    }
+
+    .open {
+      color: #000;
+
+      .logo { filter: brightness(0); }
+      .main-nav ul { display: flex; }
     }
   }
 }
