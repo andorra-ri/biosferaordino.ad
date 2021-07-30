8d0e1cfca0b629abee48287d17aefca51dc8bc43
mvilella <mvilella@obsa.ad>
2021-07-30 12:22:05 +0200

    Add Home video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
-  <p style="height: 200vh;">Home</p>
+  <div class="view-home">
+    <header class="hero">
+      <div class="video">
+        <iframe
+          :src="`https://player.vimeo.com/video/${video}?background=1&muted=1`"
+          allow="autoplay; fullscreen"
+          frameborder="0" />
+      </div>
+      <div class="container">
+        <h1>{{ t('reserva_biosfera_ordino') }}</h1>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
+import { useI18n } from 'vue-i18n';
+import { home } from '/@/config.yaml';
+
 export default {
   name: 'Home',
   setup() {
-    return {};
+    const { t } = useI18n();
+    const { video } = home;
+    return { t, video };
   },
 };
 </script>
