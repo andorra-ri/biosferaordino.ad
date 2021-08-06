91573b12d29c7aa1a594bcd8c3902fe4f33a0e43
mvilella <mvilella@obsa.ad>
2021-08-06 14:30:59 +0200

    Load Resources view

---
+++ src/views/Resources.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="view-resources">
+    <header class="hero">
+      <img src="/images/covers/resources.jpg" class="cover">
+      <div class="container">
+        <h1>{{ t('resources.tagline') }}</h1>
+      </div>
+    </header>
+  </div>
+</template>
+<script>
+import { useI18n } from 'vue-i18n';
+
+export default {
+  name: 'Resources',
+  setup() {
+    const { t } = useI18n();
+
+    return { t };
+  },
+};
+</script>
